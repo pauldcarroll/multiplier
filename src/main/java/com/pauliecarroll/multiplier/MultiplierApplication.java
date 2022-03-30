ccc0e47cddbe42b07fd8f17c38446648f7daf465
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-03-30 13:45:07 +0100

    Update MultiplierApplication.java

--- src/main/java/com/pauliecarroll/multiplier/MultiplierApplication.java
+++ src/main/java/com/pauliecarroll/multiplier/MultiplierApplication.java
@@ -11,3 +11,13 @@ public class MultiplierApplication {
 	}
 
 }
+
+package com.leszko.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+     int sum(int a, int b) {
+          return a + b;
+     }
+}

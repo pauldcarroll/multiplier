543e4f2b749a3b5748571b186d7c0888e088bc0f
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-03-30 13:50:03 +0100

    Update MultiplierController.java

--- src/main/java/com/pauliecarroll/multiplier/MultiplierController.java
+++ src/main/java/com/pauliecarroll/multiplier/MultiplierController.java
@@ -1,4 +1,4 @@
-package com.leszko.calculator;
+package com.pauliecarroll.multiplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class CalculatorController {
      @Autowired
-     private Calculator calculator;
+     private Multiplier multiplier;
 
      @RequestMapping("/sum")
-     String sum(@RequestParam("a") Integer a, 
+     String multiply(@RequestParam("a") Integer a, 
                 @RequestParam("b") Integer b) {
-          return String.valueOf(calculator.sum(a, b));
+          return String.valueOf(multiplier.multiply(a, b));
      }
 }

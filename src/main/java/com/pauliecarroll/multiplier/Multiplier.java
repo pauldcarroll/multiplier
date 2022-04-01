b5f8697b3c6f76b405ed60dc69c95947167c9d1e
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-04-01 12:51:56 +0100

    Delete src directory

--- src/main/java/com/pauliecarroll/multiplier/Multiplier.java
+++
@@ -1,9 +0,0 @@
-package com.pauliecarroll.multiplier;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Multiplier {
-     int multiply(int a, int b) {
-          return a * b;
-     }
-}
